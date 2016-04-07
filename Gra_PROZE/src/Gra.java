744b7b0cbaa8a8ce65ad01ac85f500cc6dce80df
Jacek <Jacek@DESKTOP-7GKRULC>
2016-04-07 15:30:07 +0200

    h

---
+++ Gra_PROZE/src/Gra.java
@@ -0,0 +1,76 @@
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+
+
+public class Gra extends JFrame implements ActionListener
+{
+	
+	private Gra()
+	{
+		setSize(900,600);	 
+		setTitle("Gra");
+		this.setLayout(new GridLayout(6,1));
+
+		JButton cofnij = new JButton("Cofnij");
+		JButton graj = new JButton("Graj");
+		JLabel nickNapis = new JLabel("Podaj nick");
+		JTextField nick = new JTextField();
+		JLabel poziomNapis =  new JLabel("Wybierz poziom trudnosci");
+		ButtonGroup grupa = new ButtonGroup();
+		JRadioButton niski = new JRadioButton("Niski", true);
+		JRadioButton sredni = new JRadioButton("Sredni");
+		JRadioButton wysoki = new JRadioButton("Wysoki");
+		
+		grupa.add(niski);
+		grupa.add(sredni);
+		grupa.add(wysoki);
+		this.add(niski);
+		this.add(sredni);
+		this.add(wysoki);
+		
+		this.add(cofnij);
+		
+		this.add(nickNapis);
+		this.add(nick);
+		
+		this.add(poziomNapis);
+		
+		this.add(cofnij);
+		this.add(graj);
+		
+		this.setVisible(true);
+		
+		
+	}
+	
+	private static Gra gra = new Gra();
+	public static Gra getInstance()
+	{
+		return gra;
+	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		
+	}
+
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Gra_PROZE/src/Menu.java
@@ -0,0 +1,123 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+
+
+public class Menu extends JFrame implements ActionListener
+{
+	JButton gra = new JButton("Gra");
+	JButton statystyki = new JButton("Statystyki");
+	JButton wyjscie = new JButton("Wyjscie");
+	
+	public Menu()
+	{
+		setSize(600,600);	 
+		setTitle("Space Invaders");
+		setVisible(true);
+		Panel pCENTER = new Panel();
+		
+		JPanel pWEST = new JPanel();
+		pWEST.setOpaque(true);
+		pWEST.setLayout(new BoxLayout(pWEST, BoxLayout.X_AXIS));
+		Dimension minSize1 = new Dimension(30, 50);
+		Dimension prefSize1 = new Dimension(100, 500);
+		Dimension maxSize1 = new Dimension(Short.MAX_VALUE, 1000);
+		
+		pWEST.add(new Box.Filler(minSize1, prefSize1, maxSize1));
+		Panel pEAST = new Panel();
+		pEAST.setLayout(new BoxLayout(pEAST, BoxLayout.X_AXIS));
+		pEAST.add(new Box.Filler(minSize1, prefSize1, maxSize1));
+
+		pWEST.setOpaque(true);
+		
+		gra.setPreferredSize(new Dimension(100, 30));
+		statystyki.setPreferredSize(new Dimension(100, 30));
+		wyjscie.setPreferredSize(new Dimension(100, 30));
+
+		pCENTER.setLayout(new BoxLayout(pCENTER, BoxLayout.Y_AXIS));
+		Dimension minSize2 = new Dimension(0, 50);
+		Dimension prefSize2 = new Dimension(200, 70);
+		Dimension maxSize2 = new Dimension(Short.MAX_VALUE, 2000);
+
+		pCENTER.add(new Box.Filler(minSize2, prefSize2, maxSize2));
+		Panel pGRA = new Panel();
+		pGRA.setLayout(new BorderLayout());
+		pGRA.add(gra, BorderLayout.CENTER);
+		pCENTER.add(pGRA);
+		pCENTER.add(new Box.Filler(minSize2, prefSize2, maxSize2));
+		Panel pSTATYSTYKI = new Panel();
+		pSTATYSTYKI.setLayout(new BorderLayout());
+		pSTATYSTYKI.add(statystyki, BorderLayout.CENTER);
+		pCENTER.add(pSTATYSTYKI);
+		pCENTER.add(new Box.Filler(minSize2, prefSize2, maxSize2));
+		Panel pWYJSCIE = new Panel();
+		pWYJSCIE.setLayout(new BorderLayout());
+		pWYJSCIE.add(wyjscie, BorderLayout.CENTER);
+		pCENTER.add(pWYJSCIE);
+		pCENTER.add(new Box.Filler(minSize2, prefSize2, maxSize2));
+
+		this.setLayout(new BorderLayout());
+		this.add(pCENTER, BorderLayout.CENTER);
+		this.add(pWEST, BorderLayout.WEST);
+		this.add(pEAST, BorderLayout.EAST);
+		this.pack();
+		
+		gra.addActionListener(this);
+		statystyki.addActionListener(this);
+		wyjscie.addActionListener(this);
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		Object zrodlo = e.getSource();
+		if(zrodlo == gra)
+		{
+			Gra.getInstance();
+
+
+		}
+		
+		else if(zrodlo == statystyki)
+		{
+			
+		}
+
+		else if(zrodlo == wyjscie)
+		{
+			dispose(); // vs EXIT_ON_CLOSE
+		}
+		
+	}
+
+
+
+
+	public static void main(String[] args)
+	{
+		Menu menu = new Menu();
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu.setVisible(true);
+	}
+
+}
+
+
