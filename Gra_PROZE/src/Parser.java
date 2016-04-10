304598f8e1189b198d79331ce888d9b17e24f0cc
Atakama <igomar14@wp.pl>
2016-04-10 18:15:46 +0200

    Create Parser.java

---
+++ Gra_PROZE/src/Parser.java
@@ -0,0 +1,128 @@
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.imageio.ImageIO;
+
+public class Parser
+{
+	
+	public static BufferedImage obcy,gracz;
+	ArrayList<String> lista1 = new ArrayList<String>(); 
+	ArrayList<String> lista2 = new ArrayList<String>(); 
+	ArrayList<String> lista3 = new ArrayList<String>();
+	ArrayList<String> lista4 = new ArrayList<String>();
+	static int [] lista5 = new int[12];
+	
+	
+	Parser()
+	{		
+			try
+			{
+				obcy  = ImageIO.read(new File("Sources/space-invaders-md.png"));
+				gracz = ImageIO.read(new File("Sources/ship_up.png"));
+			}
+			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			
+			/*try{
+				  FileInputStream fstream = new FileInputStream("plikKonfiguracyjny.txt");
+				  DataInputStream in = new DataInputStream(fstream);
+				  BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				  String strLine;
+				 
+				  while ((strLine = br.readLine()) != null)   {
+				     
+					  String[] splitted = strLine.split(" "); 
+					  System.out.println(splitted.toString());
+				  }
+				  
+				  in.close();
+				    }catch (Exception e){//Catch exception if any
+				  System.err.println("Error: " + e.getMessage());
+				  }*/
+			
+			
+			File plik = new File("plikKonfiguracyjny.txt");
+			try
+			{
+				Scanner odczyt = new Scanner(plik);
+				StringTokenizer token;
+				while(odczyt.hasNextLine())
+				{
+					token = new StringTokenizer(odczyt.nextLine(), " ");
+					while(token.hasMoreElements())
+					{
+						lista1.add(token.nextToken());
+					}
+				}
+			} catch (FileNotFoundException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+						
+			for(int i = 0; i < lista1.size(); i++)
+			{
+				if(i%2 == 1)
+				{
+					
+					lista2.add(lista1.get(i));
+				
+				}
+			}			
+
+			for(int i = 0; i < lista2.size() - 3; i++)
+			{
+				lista3.add(lista2.get(i));
+			}
+						
+			StringTokenizer token2;
+
+			for(int i = lista2.size() - 3; i < lista2.size(); i++)
+			{
+				token2 = new StringTokenizer(lista2.get(i), "x");
+				while(token2.hasMoreElements())
+				{
+					lista4.add(token2.nextToken());
+				}
+			}
+			
+			for(int i = 1; i <  lista3.size(); i++ )
+			{
+				lista5[i - 1] =	Integer.parseInt(lista3.get(i));
+			}
+			
+			for(int i = 0; i <  lista4.size(); i++ )
+			{
+				lista5[i+6] =	Integer.parseInt(lista4.get(i));
+			}
+			
+	}
+		
+
+		
+	public static void main(String[] args)
+	{
+		Parser parser = new Parser();
+		//System.out.println( Arrays.toString(lista5));
+
+
+	}
+
+}
