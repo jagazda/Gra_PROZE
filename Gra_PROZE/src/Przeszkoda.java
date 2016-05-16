9c5381a0e6f4118f05df0591ad82c59574136fb5
Atakama <igomar14@wp.pl>
2016-05-16 12:25:42 +0200

    Create Przeszkoda.java

---
+++ Gra_PROZE/src/Przeszkoda.java
@@ -0,0 +1,22 @@
+public class Przeszkoda
+{
+	Integer x, y, zycie;
+	boolean b;
+	
+	Przeszkoda(Integer xx, Integer yy)
+	{
+		this.x = xx;
+		this.y = yy;
+		zycie = 3;
+		b = true;
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
