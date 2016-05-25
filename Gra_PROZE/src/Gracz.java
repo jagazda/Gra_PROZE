5adef82ff941f34fb586c95b4e14bca72e4acdbc
Atakama <igomar14@wp.pl>
2016-05-25 22:34:57 +0200

    Update Gracz.java

--- Gra_PROZE/src/Gracz.java
+++ Gra_PROZE/src/Gracz.java
@@ -1,25 +1,61 @@
-import java.awt.Graphics;
-import java.awt.image.ImageObserver;
+/*
+ * klasa implementujaca obiekt gracza
+ */
 
 public class Gracz  
 {
-
-	int x = 20;
-	int y = 20;
-	int  zycia;
+	/*
+	 * wspolrzedna poczatkowa x
+	 */
+    int x;
+	/*
+	 * wspolrzedna poczatkowa y
+	 */
+     int y;
+	/*
+	 * zycie poczatkowe
+	 */
+    int zycia;
+	/*
+	 * konstruktor gracza
+	 */
 	
-	public Gracz()
+    int punkty;
+	
+    int oslona;
+    
+    int atakCzasowy;
+	
+	int dx;
+	
+	boolean alive = true;
+	
+	Parser parser = new Parser();
+	
+	private Gracz()
 	{
 		zycia = 3;
+		dx = 10;
+		punkty = 0;
+		oslona = 0;
+		atakCzasowy = 0;
+		parser.load();
+        x = Integer.parseInt(parser.properties.getProperty("xPolozenieGracza"));
+	 	y = Integer.parseInt(parser.properties.getProperty("yPolozenieGracza"));
+		
 		//setBounds(Animacja.WIDTH / 2 - Parser.obcy.getWidth() / 2, Animacja.HEIGHT - 10, Parser.obcy.getWidth(), Parser.obcy.getHeight());
 	}
 	
-	/*public void draw(Graphics g)
+	private static Gracz gracz = new Gracz();
+	public static Gracz getInstance()
 	{
-		g.drawImage(Parser.gracz, x, y,  (ImageObserver) this);
+		return gracz;
 	}
-	*/
-
+	
+/*
+ * pusta funkcja main
+ * @param args nie uzywane
+ */
 	public static void main(String[] args)
 	{
 		
