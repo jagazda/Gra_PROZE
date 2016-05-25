089ee90345769ea3f15a1ce6357783742961f73d
Atakama <igomar14@wp.pl>
2016-05-25 22:38:20 +0200

    Update Przeszkoda.java

--- Gra_PROZE/src/Przeszkoda.java
+++ Gra_PROZE/src/Przeszkoda.java
@@ -1,8 +1,21 @@
+/*
+ * klasa implementujaca przeszkode
+ */
 public class Przeszkoda
 {
+	/*
+	 * wspolrzedne przeszkody oraz zycie przeszkody
+	 */
 	Integer x, y, zycie;
+	/*
+	 * widocznosc
+	 */
 	boolean b;
-	
+	/*
+	 * konstruktor przeszkody
+	 * @param xx wspolrzedna x przeszkody
+	 * @param yy wspolrzedna y przeszkody
+	 */
 	Przeszkoda(Integer xx, Integer yy)
 	{
 		this.x = xx;
@@ -12,7 +25,10 @@ public class Przeszkoda
 	}
 	
 	
-	
+	/*
+	 * pusta funkcja main
+	 * @param args nie uzywane
+	 */
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
