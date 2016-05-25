93c60de23805048d746ba8eea34c706318856a75
Atakama <igomar14@wp.pl>
2016-05-25 22:32:57 +0200

    Create Bonusy.java

---
+++ Gra_PROZE/src/Bonusy.java
@@ -0,0 +1,24 @@
+
+public class Bonusy
+{
+
+	int x, y;
+	int czasWidocznosci;
+	int ilosc;
+	boolean isVisible = false;
+	
+	public Bonusy()
+	{
+		x = 0;
+		y = 0;
+		czasWidocznosci = 0;
+		ilosc = 0;
+	}
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
